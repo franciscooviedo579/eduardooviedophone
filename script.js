33831a68c248e7d61a27b678d2819cc855404c6b
franciscooviedo579 <74509181+franciscooviedo579@users.noreply.github.com>
2023-11-22 22:18:16 -0300

    second commit
    agregue el boton de menu

---
+++ script.js
@@ -0,0 +1,71 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menubox = document.querySelector(".menubox");
+  const nav = document.querySelector("nav");
+  const politInt = document.querySelector(".polit-int");
+  const politArg = document.querySelector(".polit-arg");
+  const politChi = document.querySelector(".polit-chi");
+  const relArgChi = document.querySelector(".rel-arg-chi");
+  const relArgJap = document.querySelector(".rel-arg-jap");
+  const relArgEEUU = document.querySelector(".rel-arg-eeuu");
+  const politIntUl = document.querySelector(".polit-int-ul");
+  const politArgUl = document.querySelector(".polit-arg-ul");
+  const politChiUl = document.querySelector(".polit-chi-ul");
+  const relArgChiUl = document.querySelector(".rel-arg-chi-ul");
+  const relArgJapUl = document.querySelector(".rel-arg-jap-ul");
+  const relArgEEUUUl = document.querySelector(".rel-arg-eeuu-ul");
+  menubox.addEventListener("click", () => {
+    if (nav.style.display === "block") {
+      nav.style.display = "none";
+    } else {
+      nav.style.display = "block";
+    }
+  });
+
+  politInt.addEventListener("click", () => {
+    if (politIntUl.style.display === "block") {
+      politIntUl.style.display = "none";
+    } else {
+      politIntUl.style.display = "block";
+    }
+  });
+
+  politArg.addEventListener("click", () => {
+    if (politArgUl.style.display === "block") {
+      politArgUl.style.display = "none";
+    } else {
+      politArgUl.style.display = "block";
+    }
+  });
+
+  politChi.addEventListener("click", () => {
+    if (politChiUl.style.display === "block") {
+      politChiUl.style.display = "none";
+    } else {
+      politChiUl.style.display = "block";
+    }
+  });
+
+  relArgChi.addEventListener("click", () => {
+    if (relArgChiUl.style.display === "block") {
+      relArgChiUl.style.display = "none";
+    } else {
+      relArgChiUl.style.display = "block";
+    }
+  });
+
+  relArgJap.addEventListener("click", () => {
+    if (relArgJapUl.style.display === "block") {
+      relArgJapUl.style.display = "none";
+    } else {
+      relArgJapUl.style.display = "block";
+    }
+  });
+
+  relArgEEUU.addEventListener("click", () => {
+    if (relArgEEUUUl.style.display === "block") {
+      relArgEEUUUl.style.display = "none";
+    } else {
+      relArgEEUUUl.style.display = "block";
+    }
+  });
+});
